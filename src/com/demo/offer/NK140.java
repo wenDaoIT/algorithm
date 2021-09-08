24302ca8e10f576b9960d75d5964c3318911d452
wenDaoIT <1362449059@qq.com>
2021-09-08 16:53:27 +0800

    +选择排序

--- src/com/demo/offer/NK140.java
+++ src/com/demo/offer/NK140.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class NK140 {
 
     public static void main(String[] args) {
-        int[] arr = {5,2,3,1,6,4};
+        int[] arr = {5, 2, 3, 1, 6, 4};
         NK140 n1 = new NK140();
 //        for (int res : n1.MySort(arr)){
 //            System.out.println(res);
@@ -16,45 +16,52 @@ public class NK140 {
 //             ) {
 //            System.out.println(res);
 //        }
-        int[] temp = new int[arr.length];
-        for (int res : n1.mergeSort(arr,0, arr.length-1,temp)){
+//        int[] temp = new int[arr.length];
+//        for (int res : n1.mergeSort(arr,0, arr.length-1,temp)){
+//            System.out.println(res);
+//        }
+        for (int res : n1.SelectSortDG(arr, 0)) {
             System.out.println(res);
+
         }
     }
-     //调用库函数Arrays.sort
-     public int[] MySort (int[] arr) {
-     Arrays.sort(arr);
+
+    //调用库函数Arrays.sort
+    public int[] MySort(int[] arr) {
+        Arrays.sort(arr);
 
 
-     return arr;
-     }
-     //冒泡排序BubbleSort
-     public int[] BubbleSort (int[] arr) {
-     int x;
-     for (int j = 0;j <arr.length-1; j++) {
-         for (int i = 0; i <arr.length-1; i++) {
-     if (arr[i]>arr[i+1]){
-     x=arr[i];
-     arr[i]=arr[i+1];
-     arr[i+1]=x;
-     }
-     }
-     }
-     return arr;
-     }
-     public int[] quickSort(int[] list, int left, int right) {
-     if (left < right) {
-     // 分割数组，找到分割点
-     int point = partition(list, left, right);
-     // 递归调用，对左子数组进行快速排序
-     quickSort(list, left, point - 1);
-     // 递归调用，对右子数组进行快速排序
-     quickSort(list, point + 1, right);
-     }
-     return list;
-     }
+        return arr;
+    }
 
-     /**
+    //冒泡排序BubbleSort
+    public int[] BubbleSort(int[] arr) {
+        int x;
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    x = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = x;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[] quickSort(int[] list, int left, int right) {
+        if (left < right) {
+            // 分割数组，找到分割点
+            int point = partition(list, left, right);
+            // 递归调用，对左子数组进行快速排序
+            quickSort(list, left, point - 1);
+            // 递归调用，对右子数组进行快速排序
+            quickSort(list, point + 1, right);
+        }
+        return list;
+    }
+
+    /**
      * 分割数组，找到分割点
      */
     public int partition(int[] list, int left, int right) {
@@ -85,44 +92,86 @@ public class NK140 {
         arr[j] = temp;
     }
 
-    public int[] mergeSort(int list[],int left,int right,int[] temp){
-        if (left<right){
+    public int[] mergeSort(int list[], int left, int right, int[] temp) {
+        if (left < right) {
             //
-            int mid = left+((right-left)>>1);
+            int mid = left + ((right - left) >> 1);
             //向左递归
-            mergeSort(list,left,mid,temp);
+            mergeSort(list, left, mid, temp);
             //向右递归
-            mergeSort(list,mid+1,right,temp);
+            mergeSort(list, mid + 1, right, temp);
             //合并
-            merge(list,left,mid,right,temp);
+            merge(list, left, mid, right, temp);
         }
         return list;
     }
-    private void merge(int[] list,int left,int mid,int right,int[] temp){
-        int t=0;//temp的当前索引
-        int i=left;
+
+    private void merge(int[] list, int left, int mid, int right, int[] temp) {
+        int t = 0;//temp的当前索引
+        int i = left;
         //新的右边界
-        int r=mid+1;
-        while (i<=mid && r<=right){
-            if (list[i]<list[r]){
+        int r = mid + 1;
+        while (i <= mid && r <= right) {
+            if (list[i] < list[r]) {
                 temp[t++] = list[i++];
-            }else {
+            } else {
                 temp[t++] = list[r++];
             }
 
         }
-        while (i<=mid){
+        while (i <= mid) {
             temp[t++] = list[i++];
         }
-        while (r<=right){
+        while (r <= right) {
             temp[t++] = list[r++];
         }
-        t=0;
-        int tempLeft =left;
-        while (tempLeft<=right){
+        t = 0;
+        int tempLeft = left;
+        while (tempLeft <= right) {
             list[tempLeft++] = temp[t++];
         }
     }
+
+    private int[] SelectSort(int[] array) {
+        //运行时间
+        //13ms
+        //占用内存
+        //11504KB
+        for (int i = 0; i < array.length; i++) {
+            int index = i;
+            //这里j=i+1 遍历过的就不用比较了
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[index] > array[j]) {
+                    index = j;
+                }
+            }
+            //加个判断
+            if (index != i) {
+                swap(array, i, index);
+            }
+        }
+        return array;
+    }
+
+    private int[] SelectSortDG(int[] array, int index) {
+        int min = index, i = index + 1;
+        if (0 == array.length) {
+            return null;
+        }
+        if (index == array.length) {
+            System.out.println("判断");
+            return null;
+        }
+        for (; i < array.length; i++) {
+            if (array[min] > array[i]) {
+                min = i;
+            }
+        }
+        if (min != index) {
+            swap(array, index, min);
+        }
+        SelectSortDG(array, ++index);
+        return array;
+    }
+
 }
-
-
